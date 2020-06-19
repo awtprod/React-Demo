e694b11344b4003773edd5136ad7193a5217742e
Andrew Ryan <ryanan@oregonstate.edu>
2020-06-19 11:14:54 -0400

    Change greeting

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3001;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World from antmedia');
+    res.end('Hello World from change');
 });
 
 server.listen(port, () => {
