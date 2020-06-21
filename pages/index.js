9ef4ea6b9a578c238e392177cddd1c62b0d5e400
Andrew Ryan <ryanan@oregonstate.edu>
2020-06-21 15:31:48 -0400

    removed import from react-router

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 class Square extends React.Component {
     render() {
