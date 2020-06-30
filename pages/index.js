f20db1268da7dd1d2145775ba41c25dff495234c
Andrew Ryan <ryanan@oregonstate.edu>
2020-06-30 16:27:25 -0400

    Fixed errors in renderSquare function.

--- pages/index.js
+++ pages/index.js
@@ -19,8 +19,12 @@ class Board extends React.Component {
         };
     }
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
-        onClick = {() => this.handleClick(i)}
+        return (
+            <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+        );
     }
 
     render() {
